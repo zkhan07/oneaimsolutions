f30c516f6a160560c0c699c2e83c40d1d07c8890
zaid <khanzaid786786.kz@gmail.com>
2020-01-23 17:30:05 +0530

    Updates

---
+++ precache-manifest.a282c15141c5e02b86d97a44dd8778a6.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f4b05889920bf277d26d4650f8171f",
+    "url": "/oneaimsolutions/index.html"
+  },
+  {
+    "revision": "1ca8366f9df47099ca40",
+    "url": "/oneaimsolutions/static/css/main.4631ae6b.chunk.css"
+  },
+  {
+    "revision": "689492059bcf45d8d6be",
+    "url": "/oneaimsolutions/static/js/2.c687e631.chunk.js"
+  },
+  {
+    "revision": "1ca8366f9df47099ca40",
+    "url": "/oneaimsolutions/static/js/main.757bc159.chunk.js"
+  },
+  {
+    "revision": "fc98bb0acc67b0953219",
+    "url": "/oneaimsolutions/static/js/runtime~main.113325dd.js"
+  },
+  {
+    "revision": "d6684974374672c98ad11a9cecbd210c",
+    "url": "/oneaimsolutions/static/media/1.d6684974.jpg"
+  },
+  {
+    "revision": "477d0ec715aaae35bacec140278b80a3",
+    "url": "/oneaimsolutions/static/media/2.477d0ec7.jpg"
+  },
+  {
+    "revision": "8d1ca407f2a7eac8874734acbdeaeb36",
+    "url": "/oneaimsolutions/static/media/3.8d1ca407.jpg"
+  },
+  {
+    "revision": "d15075d2bbe0978efc47c6865cb37076",
+    "url": "/oneaimsolutions/static/media/4.d15075d2.jpg"
+  },
+  {
+    "revision": "4722005fd24f92c51e642c70cbbbc7e8",
+    "url": "/oneaimsolutions/static/media/5.4722005f.jpg"
+  },
+  {
+    "revision": "649105a6f7cc6217e7b8bb5ab0a9912e",
+    "url": "/oneaimsolutions/static/media/6.649105a6.jpg"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/oneaimsolutions/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/oneaimsolutions/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "ad8a850235c05fe3f200960879bb4ed6",
+    "url": "/oneaimsolutions/static/media/ceo.ad8a8502.png"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/oneaimsolutions/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/oneaimsolutions/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/oneaimsolutions/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/oneaimsolutions/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/oneaimsolutions/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/oneaimsolutions/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "1331ba97560cc13103d379a8843ad1dc",
+    "url": "/oneaimsolutions/static/media/federico-beccari.1331ba97.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/oneaimsolutions/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/oneaimsolutions/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/oneaimsolutions/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "8a26aabcc3fe4dd8289d9f379c9f48c6",
+    "url": "/oneaimsolutions/static/media/joshua-stannard.8a26aabc.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/oneaimsolutions/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/oneaimsolutions/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/oneaimsolutions/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "7a84b8b3a67730307dc5e2bb806f7756",
+    "url": "/oneaimsolutions/static/media/mam.7a84b8b3.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/oneaimsolutions/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/oneaimsolutions/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/oneaimsolutions/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/oneaimsolutions/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/oneaimsolutions/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "c015a8ee6d7f4c9311081f914ff89461",
+    "url": "/oneaimsolutions/static/media/sir.c015a8ee.png"
+  },
+  {
+    "revision": "29df8117c07250e9b1ea8e3a67627f67",
+    "url": "/oneaimsolutions/static/media/soroush-karimi.29df8117.jpg"
+  },
+  {
+    "revision": "c854c1de0e1dd02b195358ee73bffa79",
+    "url": "/oneaimsolutions/static/media/team-1.c854c1de.jpg"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/oneaimsolutions/static/media/uriel-soberanes.fe3a7838.jpg"
+  }
+]);
